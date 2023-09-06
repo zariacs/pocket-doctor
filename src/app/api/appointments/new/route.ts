d6c9ef82597c41f2db14a0519b2c88f490e39208
Zaria Chen Shui <zariachenshui@gmail.com>
2023-09-05 19:41:23 -0500

    Constructed appointment creation API

---
+++ src/app/api/appointments/new/route.ts
@@ -0,0 +1,31 @@
+import prisma from "@/lib/prisma";
+import { currentUser } from "@clerk/nextjs";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+    try {
+        // Gets current user's ID
+        const patient = await currentUser();
+        if (!patient) throw new Error('No user found')
+        const patientId = patient?.id;
+
+        // Prepares appointment date and time details
+        const aptData = await request.json();
+        const aptDate = aptData.date as Date;
+        const aptTimeSlot = aptData.timeSlot as string;
+
+        // Create a new appointment in the database
+        const appointment = await prisma.appointment.create({
+          data: {
+            date: aptDate,
+            timeSlot: aptTimeSlot,
+            patientId: patientId
+          },
+        });
+  
+        return NextResponse.json(appointment, { status: 201 });
+      } catch (error) {
+        console.error(error);
+        return NextResponse.json({ error: 'Error creating appointment' }, { status: 500 });
+      }
+  };
