02f6d2db2498a17a305c745cb3e965aac5444f60
Zaria Chen Shui <zariachenshui@gmail.com>
2023-09-06 05:24:51 -0500

    Stored afterSignIn/SignUp URLs in environment variables instead

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,9 @@ import { SignIn } from "@clerk/nextjs";
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <SignIn afterSignInUrl={"/components/appointments"} />
+      <SignIn
+        afterSignInUrl={process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL}
+      />
     </main>
   );
 }
--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,10 @@ import { SignUp } from "@clerk/nextjs";
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <SignUp afterSignUpUrl={"/components/patients/new"} />
+      {/* <SignUp afterSignUpUrl={"/components/patients/new"} /> */}
+      <SignUp
+        afterSignUpUrl={process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL}
+      />
     </main>
   );
 }
